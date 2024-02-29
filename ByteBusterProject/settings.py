6bbe8a6feab3a3e2b159a1d0068e101947762d2a
Ali Iktider <aisayam.sayam@gmail.com>
2024-02-29 01:28:24 -0500

    KAN-48 Developed the front end of the sign up page and connected with backend.

--- ByteBusterProject/settings.py
+++ ByteBusterProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ByteBusterApp.apps.BytebusterappConfig',
-    "accounts.apps.AccountsConfig"
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
